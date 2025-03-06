3845a8db9e71de733f2d41c215a414388d1a7d91
aeen bolandian <bolandian85@gmail.com>
2025-03-06 14:42:37 +0330

    json added to build.gradle

--- BreakingNews/src/main/java/AP/Infrastructure.java
+++ BreakingNews/src/main/java/AP/Infrastructure.java
@@ -1,11 +1,13 @@
 package AP;
 
+import org.json.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
+
 
 public class Infrastructure {
 
