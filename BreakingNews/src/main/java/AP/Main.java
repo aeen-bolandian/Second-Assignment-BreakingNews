a9790597a444302b7c867270adb4e242033dccf8
aeen bolandian <bolandian85@gmail.com>
2025-03-06 17:25:55 +0330

    main method completed

--- BreakingNews/src/main/java/AP/Main.java
+++ BreakingNews/src/main/java/AP/Main.java
@@ -2,6 +2,8 @@ package AP;
 
 public class Main {
     public static void main(String[] args) {
-
+        String API_KEY = "84a6150bee504d0a9f2a62aedfb226d8";
+        Infrastructure infrastructure = new Infrastructure(API_KEY);
+        infrastructure.displayNewsList();
     }
 }
